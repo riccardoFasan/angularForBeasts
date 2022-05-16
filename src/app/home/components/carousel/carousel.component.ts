18502bde3d0e985ff65bf812c09d2ac6c10083c1
Riccardo Fasan <fasanriccardo21@gmail.com>
2022-05-16 14:23:59 +0200

    fix: subsink

--- src/app/home/components/carousel/carousel.component.ts
+++ src/app/home/components/carousel/carousel.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { EventName } from '@app/shared/enums/event-name.enum';
 import { Photo } from '@app/shared/models';
 import { ApiService, EventBusService } from '@app/shared/services';
 import { BehaviorSubject, Observable, Subject, switchMap } from 'rxjs';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.css'],
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent implements OnInit, OnDestroy {
   private readonly defaultAlbum: number = 1;
   private album: BehaviorSubject<number> = new BehaviorSubject<number>(
     this.defaultAlbum
@@ -19,11 +20,17 @@ export class CarouselComponent implements OnInit {
     switchMap((id: number) => this.api.getPhotosOfAlbum(id))
   );
 
+  private subSink: SubSink = new SubSink();
+
   constructor(private api: ApiService, private bus: EventBusService) {}
 
   ngOnInit(): void {
-    this.bus.on(EventName.AlbumLoaded, (value: number) => {
+    this.subSink.sink = this.bus.on(EventName.AlbumLoaded, (value: number) => {
       this.album.next(value);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subSink.unsubscribe();
   }
 }
